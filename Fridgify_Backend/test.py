11b6a144514cf319ecdbd3f07152967f1a8055e7
Joschua <joschua.goetz@gmail.com>
2019-11-11 12:03:39 +0100

    Update test.py

--- Fridgify_Backend/test.py
+++ Fridgify_Backend/test.py
@@ -2,9 +2,10 @@ from django.test import TestCase
 from Fridgify_Backend import view
 import json
 
+
 class ApiTestCases(TestCase):
 
     """Hello World test case"""
     def test_hello_world(self):
         response = json.loads(view.hello_world().content)
-        self.assertEqual(response["message"], "Hello Worl2d", "Hello world")
+        self.assertEqual(response["message"], "Hello World", "Hello world")
