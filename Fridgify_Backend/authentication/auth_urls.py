656240273823239c20e7cce61152fad92b17272a
DonkeyCo <duc.vongoc01@gmail.com>
2019-11-11 13:31:35 +0100

    Add API endpoints to project. Create folder structure.

---
+++ Fridgify_Backend/authentication/auth_urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+
+urlpatterns = [
+    path('register/', ),
+    path('login/', ),
+    path('token/', ),
+]
--- Fridgify_Backend/urls.py
+++ Fridgify_Backend/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from Fridgify_Backend import view
+from Fridgify_Backend.authentication import auth_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', view.hello_world, )
+    path('', view.hello_world, ),
+    path('auth/', include(auth_urls))
 ]
