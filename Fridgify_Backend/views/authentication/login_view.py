d6bd51178390cbe0123ac91d0705b2add700a813
DonkeyCo <duc.vongoc01@gmail.com>
2019-11-12 19:25:10 +0100

    FRY-35 Backend Authentication - Login Dummy - Started implementing Login View - Differs between POST and other methods and send a 405 when not POST - If POST, currently returns a "string"

--- Fridgify_Backend/views/authentication/login_view.py
+++ Fridgify_Backend/views/authentication/login_view.py
@@ -2,4 +2,9 @@ from django.http import HttpResponse
 
 
 def hello_world(request):
-    return HttpResponse("This is the login request page")
+    if request.method == 'POST':
+        return HttpResponse("This is the login request page")
+    else:
+        res = HttpResponse(status=405, reason="Method not allowed.")
+        res["Allow"] = "POST"
+        return res
