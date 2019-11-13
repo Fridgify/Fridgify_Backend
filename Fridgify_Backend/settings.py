89024798859163897d0bd4c1a849aa0d618ffe4c
DonkeyCo <duc.vongoc01@gmail.com>
2019-11-13 14:42:44 +0100

    change database to postgres

--- Fridgify_Backend/settings.py
+++ Fridgify_Backend/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Fridgify_Backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'backend_dev',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
