66149da0e3be82d0ba7eef7085bdfe05e0c03cc3
Duc Vo Ngoc <duc.vongoc01@gmail.com>
2019-11-15 22:33:53 +0100

    Started development of authentication system

--- Fridgify_Backend/models/accesstokens.py
+++ Fridgify_Backend/models/accesstokens.py
@@ -4,4 +4,8 @@ from django.db import models
 class Accesstokens(models.Model):
     token_id = models.AutoField(primary_key=True, unique=True)
     accesstoken = models.TextField()
+    provider_id = models.ForeignKey("Providers", on_delete=models.CASCADE)
+    valid_till = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     user_id = models.ForeignKey('Users', on_delete=models.CASCADE)
--- Fridgify_Backend/views/authentication/login.py
+++ Fridgify_Backend/views/authentication/login.py
@@ -1,5 +1,29 @@
 from django.http import JsonResponse
+from django.http import HttpResponse
+import secrets
 
 
 def entry_point(request):
-    return JsonResponse({"message": "login"})
+    if request.method == "POST":
+        login(request)
+        return HttpResponse(status=200, content="Hello")
+    else:
+        return error_response()
+
+
+def login(request):
+    print(request)
+    # Step 1. Authorization Header Check
+    if "Authorization" in request.headers:
+        # Step 1.A Check if Token is valid
+        print("Authorization header exists...")
+    else:
+        # Step 1.B Check User Credentials
+        print("Check credentials...")
+    # Step 1. Create a Token
+
+
+def error_response():
+    res = HttpResponse(status=405, reason="Method not allowed.")
+    res["Allow"] = "POST"
+    return res
