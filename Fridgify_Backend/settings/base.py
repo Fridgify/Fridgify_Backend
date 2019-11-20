6d7c690c5b7523dae2dbe96274a9fe86a9818b5e
DonkeyCo <duc.vongoc01@gmail.com>
2019-11-20 14:17:31 +0100

    documentation

--- Fridgify_Backend/settings/base.py
+++ Fridgify_Backend/settings/base.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'Fridgify_Backend'
+    'Fridgify_Backend',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
