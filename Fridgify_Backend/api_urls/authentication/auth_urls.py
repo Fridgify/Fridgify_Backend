41334d3d62bc5d1a7214cef01c4b2fe4c5f0b65a
Joschua <joschua.goetz@gmail.com>
2019-11-22 19:43:44 +0100

    Add endpoint to create dummy users.

--- Fridgify_Backend/api_urls/authentication/auth_urls.py
+++ Fridgify_Backend/api_urls/authentication/auth_urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from Fridgify_Backend.views.authentication import login
 from Fridgify_Backend.views.authentication import register
 from Fridgify_Backend.views.authentication import token
+from Fridgify_Backend.utils.init_dummy_user import create_dummyuser
 
 urlpatterns = [
     # POST
@@ -11,4 +12,7 @@ urlpatterns = [
     path('login/', login.entry_point),
     # GET
     path('token/', token.entry_point),
+    # INFO Temporary for dummy users
+    path('dummy/', create_dummyuser)
+
 ]
