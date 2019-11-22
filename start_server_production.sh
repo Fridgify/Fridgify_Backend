b5f7a19529c524b8f49dc1a9d5c30fe933bfd3a4
Joschua <joschua.goetz@gmail.com>
2019-11-22 10:03:15 +0100

    Change verbosity of backend

--- start_server_production.sh
+++ start_server_production.sh
@@ -3,5 +3,5 @@ python manage.py makemigrations Fridgify_Backend --settings=Fridgify_Backend.set
 echo "Made Migrations"
 python manage.py migrate Fridgify_Backend --settings=Fridgify_Backend.settings.production
 echo "Migrate changes"
-python manage.py runserver 0.0.0.0:9000 --settings=Fridgify_Backend.settings.production
+python manage.py runserver 0.0.0.0:9000 --verbosity 3 --settings=Fridgify_Backend.settings.production
 echo "Started server"
