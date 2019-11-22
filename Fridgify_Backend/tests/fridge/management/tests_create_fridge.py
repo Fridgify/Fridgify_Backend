da645b0576f8d3df10e2334b6c55af0ddce8b1e0
Joschua <joschua.goetz@gmail.com>
2019-11-22 22:47:40 +0100

    Create test for the create fridge endpoint

--- Fridgify_Backend/tests/fridge/management/tests_create_fridge.py
+++ Fridgify_Backend/tests/fridge/management/tests_create_fridge.py
@@ -1,11 +1,66 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from rest_framework import status
+from Fridgify_Backend.models.users import Users
+from Fridgify_Backend.models.providers import Providers
+from Fridgify_Backend.models.fridges import Fridges
+from Fridgify_Backend.models.user_fridge import UserFridge
 from Fridgify_Backend.views.fridge.management import create_fridge
+from Fridgify_Backend.views.authentication import login
+
 import json
+import datetime
 
 
 class ManagementTestCasesCreateFridge(TestCase):
 
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        user = Users.objects.create(username="dummy_name", name="Dummy", surname="Name", email="dummy@dumdum.dum",
+                             password="$2b$12$1hKYhKg4AU54eyES8qjRYOjInIgObjn0JJ8SlWPOpR9MzKcseMDVS",
+                             birth_date=datetime.date(2000, 1, 1))
+        Providers.objects.create(name="Fridgify")
+        Providers.objects.create(name="Fridgify-API")
+        fridge = Fridges.objects.create(name="Miau", description="adsdsadad")
+        request = self.factory.post("/auth/login/", {"username": "dummy_name", "password": "password"},
+                                    content_type="application/json")
+        UserFridge.objects.create(fridge_id=fridge.fridge_id, user_id=user.user_id)
+        self.token = json.loads(login.login(request).content)["token"]
+
     """Create fridge test case"""
-    def test_create_fridge(self):
-        response = json.loads(create_fridge.entry_point("dummy").content)
-        self.assertEqual(response["message"], "create fridge", "create fridge")
+    def test_create_fridge_body_validation(self):
+        request_no_body = self.factory.post("/fridge/management/create/", content_type="application/json")
+        header = {"Authorization": self.token}
+        request_no_body.__setattr__("headers", header)
+        response_no_body = create_fridge.entry_point(request_no_body)
+        self.assertEqual(response_no_body.status_code, status.HTTP_400_BAD_REQUEST, "Body is not present")
+        self.assertEqual(json.loads(response_no_body.content)["message"], "Missing parameters. Required are name and description"
+                         , "Body is not present")
+
+        request_wrong_keys = self.factory.post("/fridge/management/create/", {
+            "Cat": "Miau",
+            "askdakdmlkad": "adsdsadad"
+        }, content_type="application/json")
+        request_wrong_keys.__setattr__("headers", header)
+        response_wrong_keys = create_fridge.entry_point(request_wrong_keys)
+        self.assertEqual(response_wrong_keys.status_code, status.HTTP_400_BAD_REQUEST, "Body is not present")
+        self.assertEqual(json.loads(response_wrong_keys.content)["message"], "Missing parameters. Required are name and description"
+                         , "Body is not present")
+
+        request_duplicate_entry = self.factory.post("/fridge/management/create/", {
+            "name": "Miau",
+            "description": "adsdsadad"
+        }, content_type="application/json")
+        request_duplicate_entry.__setattr__("headers", header)
+        response_duplicate_entry = create_fridge.entry_point(request_duplicate_entry)
+        self.assertEqual(response_duplicate_entry.status_code, status.HTTP_409_CONFLICT, "Duplicate fridge")
+        self.assertEqual(json.loads(response_duplicate_entry.content)["message"], "Fridge Miau already exists for user", "Duplicate fridge")
+
+        request_correct = self.factory.post("/fridge/management/create/", {
+            "name": "Wuff",
+            "description": "adsdsadad"
+        }, content_type="application/json")
+        request_correct.__setattr__("headers", header)
+        response_correct = create_fridge.entry_point(request_correct)
+        self.assertEqual(response_correct.status_code, status.HTTP_201_CREATED, "Created fridge")
+        self.assertEqual(json.loads(response_correct.content)["message"], "Created", "Created fridge")
