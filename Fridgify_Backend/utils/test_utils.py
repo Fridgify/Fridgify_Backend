8c3322555b6da9965f33d4328388b8bece5ada68
Joschua <joschua.goetz@gmail.com>
2019-11-23 18:01:11 +0100

    Add tests for /fridges

--- Fridgify_Backend/utils/test_utils.py
+++ Fridgify_Backend/utils/test_utils.py
@@ -1,5 +1,7 @@
 import datetime
 from Fridgify_Backend.models.users import Users
+from Fridgify_Backend.models.providers import Providers
+
 
 def create_dummyuser():
     user = Users()
@@ -11,3 +13,8 @@ def create_dummyuser():
     user.password = "$2b$12$1hKYhKg4AU54eyES8qjRYOjInIgObjn0JJ8SlWPOpR9MzKcseMDVS"
     user.birth_date = datetime.date(2000, 10, 17)
     user.save()
+
+
+def create_providers():
+    Providers.objects.create(name="Fridgify")
+    Providers.objects.create(name="Fridgify-API")
