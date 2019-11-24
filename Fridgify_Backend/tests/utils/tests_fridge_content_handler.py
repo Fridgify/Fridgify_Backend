deda8efd3b4f8b2222165d2a7c5db6baec4bd8f0
Duc Vo Ngoc <duc.vongoc01@gmail.com>
2019-11-24 01:42:29 +0100

    There is a possibility, that I'm actually really stupid. If there are no more fixes for tests - I am.

--- Fridgify_Backend/tests/utils/tests_fridge_content_handler.py
+++ Fridgify_Backend/tests/utils/tests_fridge_content_handler.py
@@ -4,6 +4,7 @@ from django.test import TestCase, RequestFactory
 
 from Fridgify_Backend.utils import fridge_content_handler
 from Fridgify_Backend.models.fridges import Fridges
+from Fridgify_Backend.models.users import Users
 from Fridgify_Backend.models.fridge_content import FridgeContent
 from Fridgify_Backend.models.items import Items
 from Fridgify_Backend.tests import test_utils
@@ -16,21 +17,22 @@ class UtilsTestCaseFridgeContentHandler(TestCase):
         test_utils.setup()
         test_utils.create_dummyuser()
         Fridges.objects.create(name="Dummy Fridge", description="This is a dummy fridge")
+        test_utils.connect_fridge_user()
         test_utils.create_login_token(timezone.now() + timezone.timedelta(hours=1))
         test_utils.create_api_token(timezone.now() + timezone.timedelta(hours=1))
+        self.user = Users.objects.filter(username="dummy_name").values_list("user_id").first()[0]
+        self.fridge = Fridges.objects.filter(name="Dummy Fridge").values_list("fridge_id").first()[0]
 
     @mock.patch("Fridgify_Backend.utils.fridge_content_handler.check_item_exists")
     @mock.patch("Fridgify_Backend.utils.fridge_content_handler.get_fridge")
     def test_fridgeAddItem_ValidDataItemNotExisting_Added(self, mock_get_fridge, mock_check_exists):
-        Fridges.objects.create(name="Dummy Fridge", description="This is a dummy fridge")
-        test_utils.connect_fridge_user()
         fridge = Fridges.objects.filter(name="Dummy Fridge").first()
         mock_get_fridge.return_value = fridge
         mock_check_exists.return_value = None
         request = {"name": "Item 1", "description": "This is a item",
                    "buy_date": "2019-10-17", "expiration_date": "2019-11-23",
                    "amount": 9, "unit": "kg", "store": "Rewe"}
-        result = fridge_content_handler.fridge_add_item(1, 1, request)
+        result = fridge_content_handler.fridge_add_item(self.fridge, self.user, request)
         self.assertEqual(result, 1)
         self.assertEqual(len(FridgeContent.objects.filter(item__name="Item 1")), 1)
         self.assertEqual(len(Items.objects.filter(name="Item 1")), 1)
@@ -38,14 +40,12 @@ class UtilsTestCaseFridgeContentHandler(TestCase):
     @mock.patch("Fridgify_Backend.utils.fridge_content_handler.check_item_exists")
     @mock.patch("Fridgify_Backend.utils.fridge_content_handler.get_fridge")
     def test_fridgeAddItem_ValidDataItemExisting_Added(self, mock_get_fridge, mock_check_exists):
-        Fridges.objects.create(name="Dummy Fridge", description="This is a dummy fridge")
-        test_utils.connect_fridge_user()
         mock_get_fridge.return_value = Fridges.objects.filter(name="Dummy Fridge").first()
         mock_check_exists.return_value = test_utils.create_items("")
         request = {"name": "Item A", "description": "Description",
                    "buy_date": "2019-01-01", "expiration_date": "2019-01-01",
                    "amount": 1, "unit": "kg", "store": "Rewe"}
-        result = fridge_content_handler.fridge_add_item(1, 1, request)
+        result = fridge_content_handler.fridge_add_item(self.user, self.fridge, request)
         self.assertEqual(result, 1)
         self.assertEqual(len(FridgeContent.objects.filter(item__name="Item A")), 1)
         self.assertEqual(len(Items.objects.filter(name="Item A")), 1)
@@ -60,14 +60,12 @@ class UtilsTestCaseFridgeContentHandler(TestCase):
         request = {"name": "Item A", "description": "Description",
                    "buy_date": "2019-01-01", "expiration_date": "2019-01-01",
                    "amount": 1, "unit": "kg", "store": "Rewe"}
-        result = fridge_content_handler.fridge_add_item(1, 1, request)
+        result = fridge_content_handler.fridge_add_item(self.user, self.fridge, request)
         self.assertEqual(result, -1)
 
     @mock.patch("Fridgify_Backend.utils.fridge_content_handler.check_item_exists")
     @mock.patch("Fridgify_Backend.utils.fridge_content_handler.get_fridge")
     def test_fridgeAddItem_fridgeNotExisting_0(self, mock_get_fridge, mock_check_exists):
-        Fridges.objects.create(name="Dummy Fridge", description="This is a dummy fridge")
-        test_utils.connect_fridge_user()
         mock_get_fridge.return_value = Fridges.objects.filter(name="Not Fridge").first()
         mock_check_exists.return_value = -1
         request = {"name": "Item A", "description": "Description",
