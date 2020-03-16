882054dcd4e068bff00a0661d83232718411653f
DonkeyCo <duc.vongoc01@gmail.com>
2020-03-16 21:18:53 +0100

    Refactor Fridge Content endpoint
    - uses api_view
    - fewer queries are being used

--- Fridgify_Backend/utils/decorators.py
+++ Fridgify_Backend/utils/decorators.py
@@ -1,17 +1,47 @@
 import json
 from functools import wraps
 
-from rest_framework.exceptions import ParseError
+from rest_framework.exceptions import ParseError, NotAuthenticated
+
+from Fridgify_Backend.models import UserFridge
 
 
 def check_body(*keys):
     def decorator(func):
         @wraps(func)
         def wrapper(request=None, *args, **kwargs):
-            body = request.body.decode("utf-8")
+            body = json.loads(request.body.decode("utf-8"))
             for key in keys:
-                if key not in json.loads(body):
+                if key not in body:
                     raise ParseError(detail="Missing arguments")
+            return func(request, *args, **kwargs)
+        return wrapper
+    return decorator
+
+
+def check_fridge_access():
+    def decorator(func):
+        @wraps(func)
+        def wrapper(request=None, fridge_id=None, *args, **kwargs):
+            user = request.user
+            if UserFridge.objects.filter(fridge_id=fridge_id, user=user).exists():
+                return func(request, fridge_id, *args, **kwargs)
+            raise NotAuthenticated(detail="No access to fridge")
+        return wrapper
+    return decorator
+
+
+def permitted_keys(*keys):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(request=None, *args, **kwargs):
+            body = json.loads(request.body.decode("utf-8"))
+            invalid_keys = []
+            for key in keys:
+                if key in body:
+                    invalid_keys.append(key)
+            if invalid_keys:
+                raise ParseError(detail=f"{', '.join(invalid_keys)} are not permitted")
             return func(request, *args, **kwargs)
         return wrapper
     return decorator
