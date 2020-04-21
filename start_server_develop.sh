af0907adca4441a995d750047a574cc60c0b1a94
DonkeyCo <duc.vongoc01@gmail.com>
2020-04-21 18:25:45 +0200

    Fix migration error
    - set settings in script to an ENV variable

--- start_server_develop.sh
+++ start_server_develop.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+export DJANGO_SETTINGS_MODULE=Fridgify_Backend.settings.develop
 python manage.py collectstatic --no-input
 echo "Collect static files"
+python manage.py makemigrations Fridgify_Backend
+echo "Made Migrations"
+python manage.py migrate Fridgify_Backend
+echo "Migrate changes"
 python manage.py loaddata providers.json
 echo "Add fixtures"
-python manage.py makemigrations Fridgify_Backend --settings=Fridgify_Backend.settings.develop
-echo "Made Migrations"
-python manage.py migrate Fridgify_Backend --settings=Fridgify_Backend.settings.develop
-echo "Migrate changes"
-python manage.py runserver 0.0.0.0:9999 --verbosity 3 --settings=Fridgify_Backend.settings.develop
+python manage.py runserver 0.0.0.0:9999 --verbosity 3
 echo "Started server"
--- start_server_production.sh
+++ start_server_production.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+export DJANGO_SETTINGS_MODULE=Fridgify_Backend.settings.production
 python manage.py collectstatic --no-input
 echo "Collect static files"
+python manage.py makemigrations Fridgify_Backend
+echo "Made Migrations"
+python manage.py migrate Fridgify_Backend
+echo "Migrate changes"
 python manage.py loaddata providers.json
 echo "Add fixtures"
-python manage.py makemigrations Fridgify_Backend --settings=Fridgify_Backend.settings.production
-echo "Made Migrations"
-python manage.py migrate Fridgify_Backend --settings=Fridgify_Backend.settings.production
-echo "Migrate changes"
-python manage.py runserver 0.0.0.0:9000 --verbosity 3 --settings=Fridgify_Backend.settings.production
+python manage.py runserver 0.0.0.0:9000 --verbosity 3
 echo "Started server"
