c9275005eb03a8223ed098c3568928e8f722f92d
DonkeyCo <duc.vongoc01@gmail.com>
2020-04-21 17:59:41 +0200

    Change order of script execution
    - fix a bug, where the migrations are not made

--- start_server_develop.sh
+++ start_server_develop.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-python manage.py makemigrations Fridgify_Backend --settings=Fridgify_Backend.settings.develop
-echo "Made Migrations"
-python manage.py migrate Fridgify_Backend --settings=Fridgify_Backend.settings.develop
-echo "Migrate changes"
 python manage.py collectstatic --no-input
 echo "Collect static files"
 python manage.py loaddata providers.json
 echo "Add fixtures"
+python manage.py makemigrations Fridgify_Backend --settings=Fridgify_Backend.settings.develop
+echo "Made Migrations"
+python manage.py migrate Fridgify_Backend --settings=Fridgify_Backend.settings.develop
+echo "Migrate changes"
 python manage.py runserver 0.0.0.0:9999 --verbosity 3 --settings=Fridgify_Backend.settings.develop
 echo "Started server"
--- start_server_production.sh
+++ start_server_production.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-python manage.py makemigrations Fridgify_Backend --settings=Fridgify_Backend.settings.production
-echo "Made Migrations"
-python manage.py migrate Fridgify_Backend --settings=Fridgify_Backend.settings.production
-echo "Migrate changes"
 python manage.py collectstatic --no-input
 echo "Collect static files"
 python manage.py loaddata providers.json
 echo "Add fixtures"
+python manage.py makemigrations Fridgify_Backend --settings=Fridgify_Backend.settings.production
+echo "Made Migrations"
+python manage.py migrate Fridgify_Backend --settings=Fridgify_Backend.settings.production
+echo "Migrate changes"
 python manage.py runserver 0.0.0.0:9000 --verbosity 3 --settings=Fridgify_Backend.settings.production
 echo "Started server"
