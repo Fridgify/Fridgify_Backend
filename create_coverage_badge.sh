17fa2caa30f1e2635d351f29e8225f64905b356c
JoschuaGoetz <joschua.goetz@gmail.com>
2020-05-06 17:36:15 +0200

    Let python version range from 3.7.x

--- create_coverage_badge.sh
+++ create_coverage_badge.sh
@@ -1,11 +1,1 @@
-#!/../bin/sh
-echo "create code coverage badge"
-ls
-rm -r coverage # Python image doesnt seem to have the necessary cli tools 
-mkdir coverage
-python manage.py makemigrations Fridgify_Backend --settings=Fridgify_Backend.settings.local
-python manage.py migrate Fridgify_Backend --settings=Fridgify_Backend.settings.local
-python manage.py test --settings=Fridgify_Backend.settings.local
-mv .coverage ./coverage
-cd coverage
-coverage-badge -o coverage.svg
+#!/../bin/shecho "get code"git statusecho "create code coverage badge"rm -r coverage # Python image doesnt seem to have the necessary cli tools mkdir coveragepython3 manage.py makemigrations Fridgify_Backend --settings=Fridgify_Backend.settings.localpython3 manage.py migrate Fridgify_Backend --settings=Fridgify_Backend.settings.localpython3 manage.py test --settings=Fridgify_Backend.settings.localmv .coverage ./coveragecd coveragecoverage-badge -o coverage.svg
