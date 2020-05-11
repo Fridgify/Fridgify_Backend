cef5daf64b89d0da646555af0b56e01dfd4d1793
JoschuaGoetz <joschua.goetz@gmail.com>
2020-05-11 22:30:04 +0200

    Add Dockerfiles to run tests locally

--- run_tests.sh
+++ run_tests.sh
@@ -1,13 +1,11 @@
 #!/../bin/sh
 echo "testing"
-
 python manage.py makemigrations Fridgify_Backend --settings=Fridgify_Backend.settings.testing
 python manage.py migrate Fridgify_Backend --settings=Fridgify_Backend.settings.testing
 if python manage.py test --settings=Fridgify_Backend.settings.testing; then
-    echo "Command succeeded"
-    exit 0
+  echo "Command succeeded"
+  exit 0
 else
-    echo "Command failed"
-    exit 1
+  echo "Command failed"
+  exit 1
 fi
-
