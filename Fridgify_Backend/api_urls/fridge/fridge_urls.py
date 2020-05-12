e6521b411cd12312a93ebce102ab980aab251f52
JoschuaGoetz <joschua.goetz@gmail.com>
2020-05-12 22:17:42 +0200

    Update url patterns. Remove unused files

--- Fridgify_Backend/api_urls/fridge/fridge_urls.py
+++ Fridgify_Backend/api_urls/fridge/fridge_urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from Fridgify_Backend.views.fridge import fridge
 
 from Fridgify_Backend.api_urls.fridge.content import content_urls
-from Fridgify_Backend.api_urls.fridge.item import item_urls
 from Fridgify_Backend.api_urls.fridge.management import management_urls
 
 urlpatterns = [
@@ -11,8 +10,6 @@ urlpatterns = [
     path('', fridge.fridge_view),
     # Content Endpoint
     path('content/', include(content_urls)),
-    # Item Endpoint
-    path('item/', include(item_urls)),
     # Management Endpoint
     path('management/', include(management_urls)),
 ]
