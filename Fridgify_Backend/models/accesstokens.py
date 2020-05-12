3e82c6a203e09e7b5137b178ce6e95c045b5222a
DonkeyCo <duc.vongoc01@gmail.com>
2020-05-12 20:13:20 +0200

    Adjust model Accesstoken
    - additional url_field: for redirects
    - additional fridge reference: for qr code
    - add QR provider

--- Fridgify_Backend/models/accesstokens.py
+++ Fridgify_Backend/models/accesstokens.py
@@ -7,14 +7,16 @@ class Accesstokens(models.Model):
     accesstoken = models.TextField()
     client_id = models.TextField(null=True)
     client_secret = models.TextField(null=True)
-    provider = models.ForeignKey("Providers", on_delete=models.CASCADE)
+    redirect_url = models.URLField(null=True)
     valid_till = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    provider = models.ForeignKey("Providers", on_delete=models.CASCADE)
     user = models.ForeignKey('Users', on_delete=models.CASCADE)
+    fridge = models.ForeignKey('Fridges', on_delete=models.CASCADE, null=True)
 
     class Meta:
-        unique_together = ('provider', 'user',)
+        unique_together = ('provider', 'user', 'fridge')
 
     def __dir__(self):
         return [
